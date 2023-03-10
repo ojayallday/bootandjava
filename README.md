0b8f4413e3d4472af4efb5e819ed2dbf2dacf774
Julius Ojala Ojakaa <ojayallday@gmail.com>
2023-03-11 01:00:07 +0300

    Javascript files and HTML

---
+++ README.md
@@ -0,0 +1,20 @@
+## Project Name
+-Bootstrp and Java Script
+## Author
+- Julius Ojala Ojakaa
+## Project Description
+- A simple  HTML and Javascript program executing the Akran naming convention.
+## Project Setup Instructions
+- Git Clone https://github.com/ojayallday/bootandjava.git
+- Open the cloned folder in vs code
+- Go live
+## Tools and Technologies Used
+- HTML
+- CSS
+- JAVA SCRIPT
+- VS CODE
+- GIT & GITHUB
+## Live link
+https://ojayallday.github.io/Portfolio_Landing_Page
+## Copyright and License information
+- (C) 2023 Julius Ojala Ojakaa
---
+++ scripts.js
@@ -0,0 +1,117 @@
+/**
+ * SOLID principles, Single Responsibility. Avoid repeating yourself.
+ */
+
+/**
+ * Function to get the day of the week.
+ * @param birthDate The birth date.
+ * @returns The day of the week.
+ */
+function getDayOfTheWeek(birthDate){
+    return birthDate.getDay()    
+}
+
+
+/**
+ * Function to update the ui.
+ */
+function updateUI(){
+    let birthDate = document.querySelector('input[type="date"]').value
+    birthDate = new Date(birthDate);
+    console.log(birthDate)
+    /*let birthDate;
+    document.querySelector('input[type="date"]').addEventListener("change", function() {
+        let input = this.value;
+        birthDate = new Date(input);
+    });*/
+    console.log(birthDate)
+    const gender = document.getElementById("gender").value 
+    console.log(gender)
+    const dayOfWeek = getDayOfTheWeek(birthDate);
+    const name = akranName(dayOfWeek, gender);
+    document.getElementById("result").innerHTML = name;
+}
+
+/**
+ * Function to return Akran Name from day of the week and gender inputs
+ * @param dayOfTheWeek the birthdate entered by user
+ * @param gender the gender entered by user
+ * @returns returns the Akran name
+ */
+function akranName(dayOfTheWeek, gender) {
+    let akranName;
+    if (!dayOfTheWeek) {
+        throw new Error("Day of the week has not been provided");
+    }
+
+    if (!gender) {
+        throw new Error("Gender has not been provided");
+    }
+
+    if(gender.toLowerCase() =="male"){
+        return maleAkranName(dayOfTheWeek)
+    }
+    
+    if(gender.toLowerCase() =="female"){
+        return femaleAkranName(dayOfTheWeek)
+    }
+   
+}
+
+
+/**
+ * Function to handle male Akran names.
+ * @param dayOfTheWeek The day of the week.
+ * @returns The male Akran name.
+ */
+function maleAkranName(dayOfTheWeek) {
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+
+    switch (dayOfTheWeek) {
+        case 0:
+            return maleNames[0];
+        case 1:
+            return maleNames[1];
+        case 2:
+            return maleNames[2]
+        case 3:
+            return maleNames[3];
+        case 4:
+            return maleNames[4]
+        case 5:
+            return maleNames[5]
+        case 6:
+            return maleNames[6]
+        default:
+            break;
+    }
+}
+
+/**
+ * Function to handle male Akran names.
+ * @param dayOfTheWeek The day of the week.
+ * @returns The male Akran name.
+ */
+function femaleAkranName(dayOfTheWeek) {
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+    switch (dayOfTheWeek) {
+        case 0:
+            return femaleNames[0];
+        case 1:
+            return femaleNames[1];
+        case 2:
+            return femaleNames[2]
+        case 3:
+            return femaleNames[3];
+        case 4:
+            return femaleNames[4]
+        case 5:
+            return femaleNames[5]
+        case 6:
+            return femaleNames[6]
+        default:
+            break;
+    }
+}
+
